95f3f28679dd5978af82d67a95862834b6aa5df6
Isaac <isaac@excham.net>
2018-01-28 16:28:11 +1300

    Set up bower for front end dependencies

--- 002 - Forums/views/template/head.php
+++ 002 - Forums/views/template/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <title><?=$site['title']?></title>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.css" />
+  <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="/bower_components/bulma/css/bulma.css" />
   <link rel="stylesheet" href="/css/styles.css" />
 </head>
